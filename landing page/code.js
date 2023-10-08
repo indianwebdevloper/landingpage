0887a7e7d9cefa660ba42a218e96c2b0f4886cab
indianwebdevloper <141849052+indianwebdevloper@users.noreply.github.com>
2023-10-08 21:10:36 +0530

    Add files via upload

---
+++ landing page/code.js
@@ -0,0 +1,6 @@
+// script.js
+
+// Function to handle the "Learn More" button click
+document.getElementById("learn-more").addEventListener("click", function() {
+    alert("Thank you for your interest in Company Name!");
+});
